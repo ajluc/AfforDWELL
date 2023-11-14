c054aa942d89bcc193de3e0625a781e2ee44b55b
Amaya Lucas <amayajlucas@gmail.com>
2023-11-14 16:35:21 -0500

    started search bar

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
+import { MapboxSearchBox } from '@mapbox/search-js-web';
 
 const Map = () => {
     const mapContainer = useRef(null);
@@ -122,6 +123,20 @@ const Map = () => {
                         .addTo(newMap);
                 });
 
+                // Add search bar
+                // const searchJS = document.getElementById('search-js');
+                // searchJS.onload = function () {
+                //     const searchBox = new MapboxSearchBox();
+                //     searchBox.accessToken = mapboxgl.accessToken;
+                //     searchBox.options = {
+                //         types: 'address,poi',
+                //         proximity: [-73.99209, 40.68933]
+                //     };
+                //     searchBox.marker = true;
+                //     searchBox.mapboxgl = mapboxgl;
+                //     newMap.addControl(searchBox);
+                // };
+
                 // Change the cursor to a pointer when hovering over clusters and individual markers
                 newMap.on('mouseenter', 'clusters', () => {
                     newMap.getCanvas().style.cursor = 'pointer';
@@ -148,7 +163,12 @@ const Map = () => {
 
     }, [map]);
 
-    return <div ref={mapContainer} style={{ width: '100%', height: '500px' }} />;
+    return (<div>
+                    <div id="search-js"></div>
+
+        <div ref={mapContainer} style={{ width: '100%', height: '500px' }} />
+        </div>
+        );
 };
 
 export default Map;
