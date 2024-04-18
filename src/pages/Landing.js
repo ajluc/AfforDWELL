1c5d1447bd0b19c9fb592a53b5c35297aca9a3fc
Amaya Lucas <amayajlucas@gmail.com>
2024-04-18 13:41:28 -0400

    temporary landing page copy

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,6 +1,14 @@
+import { Button, Container } from "react-bootstrap"
+
 const Landing = () => {
     return (
-        <div></div>
+        <Container>
+            <h1>Is your apartment rent stabilized?</h1>
+            <p>Search bar</p>
+            <p>We think it should be easier for New Yorkers to find out if their apartment is rent stabilized, so we put together this resource. By scraping annual tax documents and lists published by the Rental Guidelines Board, we documented how many rent stabilized units each building in NYC has. Use the search bar above to find out more about your particular apartment, or browse the map to understand what affordable options exist near you.</p>
+            <Button>View the Map</Button>
+            <Button>Learn More about Rent stabilization</Button>
+        </Container>
     )
 }
 
